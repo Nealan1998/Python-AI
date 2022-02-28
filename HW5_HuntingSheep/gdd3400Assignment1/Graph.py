154560d04b1169793a44629c1d00ad8db93575e1
Neilan <abramboyd98@gmail.com>
2022-02-28 16:56:47 -0700

    Best done

--- HW5_HuntingSheep/gdd3400Assignment1/Graph.py
+++ HW5_HuntingSheep/gdd3400Assignment1/Graph.py
@@ -230,6 +230,28 @@ class Graph():
 		# TODO: Implement Best First Search
 		while len(pqueue) != 0:
 			# pop the currNode off
+			#curNode = pqueue.pop(0)
+			#curNode.isExplored = True
+			# Check if node is endNode
+			#if curNode == endNode:
+			#	return self.buildPath(curNode)
+			# Check each neighbor
+			#for nextNode in curNode.neighbors:
+			#	curDistance = (curNode.center - nextNode.center).length()
+			#	# handle unvisited node
+			#	if nextNode.isVisited == False:
+			#		nextNode.isVisited = True
+			#		nextNode.cost = (nextNode.center - curNode.center).length()
+			#		nextNode.backNode = curNode
+			#		pqueue.append(nextNode)
+				# Handle visited node
+			#	else:
+			#		if (curDistance < nextNode.cost):
+						#nextNode.costFromStart = newCostFromStart
+						#nextNode.costToEnd = newCostToEnd
+			#			nextNode.cost = 0
+						#nextNode.backNode = curNode
+
 			curNode = pqueue.pop(0)
 			curNode.isExplored = True
 			# Check if node is endNode
@@ -241,16 +263,26 @@ class Graph():
 				# handle unvisited node
 				if nextNode.isVisited == False:
 					nextNode.isVisited = True
-					nextNode.cost = (curNode.center - nextNode.center).length()
+					#nextNode.costFromStart = curDistance + curNode.costFromStart
+					#nextNode.costToEnd = 0
+					nextNode.cost = (nextNode.center - endNode.center).length()
 					nextNode.backNode = curNode
 					pqueue.append(nextNode)
 				# Handle visited node
 				else:
-					newCost = (curNode.center - nextNode.center).length()
+				#	#ne
+				#newCostFromStart = curNode.costFromStart + curDistance
+				#	newCostToEnd = 0 #curNode.costToEnd + curDistance
+				#	newCost = newCostFromStart + newCostToEnd
+				#	newCostFromStart = curNode.costFromStart + curDistance
+				#	newCostToEnd = 0 #curNode.costToEnd + curDistance
+					newCost = (nextNode.center - endNode.center).length()
 					if (newCost < nextNode.cost):
+				#		nextNode.costFromStart = newCostFromStart
+				#		nextNode.costToEnd = newCostToEnd
 						nextNode.cost = newCost
 						nextNode.backNode = curNode
-			#pqueue.sort(key=lambda x:x.cost)
+			pqueue.sort(key=lambda x:x.cost)
 
 		
 		
